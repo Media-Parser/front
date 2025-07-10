2abc0211a787cef59b11d156d17706959a8b5c99
yujinoh0103 <yujin0103@sookmyung.ac.kr>
2025-07-10 08:01:20 +0000

    ec2 업로드

--- src/features/Trash/TrashPage.tsx
+++ src/features/Trash/TrashPage.tsx
@@ -9,7 +9,7 @@ import { groupDocumentsByDate } from "../../lib/utils/groupDocumentsByDate";
 import type { Document } from "../../types/documentType";
 import LoadingOrError from "../../components/Common/LoadingOrError";
 import useAuthStore from "../../store/useAuthStore";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import SearchBar from "../../components/Searchbar/SearchBar";
 import Button from "../../components/Button/Button";
 
 const TrashPage = () => {
