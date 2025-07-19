d1030c088e398253d95be6d99cefdc36a1c97b73
ds <rh8534@naver.com>
2025-07-19 07:36:48 +0000

    css fix

--- src/types/analyzeType.ts
+++ src/types/analyzeType.ts
@@ -3,6 +3,7 @@ export interface SentenceAnalysis {
   index: number;              // 문장 인덱스
   text: string;               // 문장 전체 텍스트
   flag: boolean;              // true(부적합) / false(적합)
+  label: string;              // 라벨
   highlighted: string[];      // 부적합일 경우 하이라이트 부분(배열)
   explanation: string[] | string;      // 사유 라벨(복수 가능)
 }
